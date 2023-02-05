87c4e1148d2bfe9e4c0d05fa3d5ba7cc88de7226
Guilherme Serafim Bonfim <guilhermeserafimbonfim@hotmail.com>
2023-02-05 10:04:20 -0300

    feat: initialize project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+// styles
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/scss/theme.scss';
+
+function App(): ReactElement {
+  return <div>Hello</div>;
+}
+
+export default App;
